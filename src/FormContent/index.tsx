dfe2ad522cdaac49fe2b31b936502e52fea69ee3
周美磊 <ZHOUMEILEI@SFMAIL.SF-EXPRESS.COM>
2022-06-14 19:50:43 +0800

    优化formContent的内容

--- src/FormContent/index.tsx
+++ src/FormContent/index.tsx
@@ -1,11 +1,11 @@
 /*
  * @Author: zml
  * @Date: 2022-06-06 13:29:04
- * @LastEditTime: 2022-06-06 13:29:18
+ * @LastEditTime: 2022-06-14 19:49:45
  */
 import { FCProps, GetIProps } from '../public';
 import { Col, Form } from 'antd';
-import type { CSSProperties } from 'react';
+import { CSSProperties, useMemo } from 'react';
 
 const FormContent: FCProps<
   GetIProps<typeof Form.Item> & {
@@ -17,11 +17,20 @@ const FormContent: FCProps<
     itemStyle?: CSSProperties;
   }
 > = (props) => {
-  const { children, span, style, itemStyle, ...itemProps } = props;
+  const { children, span, style, itemStyle, required, rules, ...itemProps } =
+    props;
+
+  const rulesConf = useMemo(() => {
+    const res = rules || [];
+    if (required) {
+      res.push({ message: `${props.label}不能为空`, required });
+    }
+    return res;
+  }, [props.label, required, rules]);
 
   return (
     <Col span={span} style={style}>
-      <Form.Item {...itemProps} style={itemStyle}>
+      <Form.Item rules={rulesConf} {...itemProps} style={itemStyle}>
         {children}
       </Form.Item>
     </Col>
