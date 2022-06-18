4f8765a4f114e2505c1fd081f59bd9ce4295d4ea
周美磊 <ZHOUMEILEI@SFMAIL.SF-EXPRESS.COM>
2022-06-18 16:56:56 +0800

    修改publish

--- publish.sh
+++ publish.sh
@@ -1,15 +1,24 @@
 ###
  # @Author: zml
  # @Date: 2022-06-06 15:51:19
- # @LastEditTime: 2022-06-18 11:40:06
+ # @LastEditTime: 2022-06-18 16:54:41
 ### 
 
-npm run build
+# npm run build
 
 if [ $? -eq 0 ]; then
   cp ts-path.txt ./es/tsconfig.json
   cp ts-path.txt ./lib/tsconfig.json
-  npm publish --registry http://npm.sfjswl.com
+  br=`git branch | grep "*"`
+
+  cmd_res=`git rev-list ${br/* /} --not origin/master`
+  echo $cmd_res
+  if [ $cmd_res == '' ]; then
+    echo 'ok!!'
+    # npm publish --registry http://npm.sfjswl.com
+  else
+    echo '当前分支落后master分支, 禁止发布'
+  fi
 else
-  echo "tsc编译异常, 请检查是否符合ts语法规范"
+  echo "tsc compile error"
 fi
