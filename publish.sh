c6a35ccc4cd0f6b08951af01f5806d87ce93c41b
周美磊 <ZHOUMEILEI@SFMAIL.SF-EXPRESS.COM>
2022-06-20 10:22:15 +0800

    测试发布脚本

--- publish.sh
+++ publish.sh
@@ -1,13 +1,12 @@
 ###
  # @Author: zml
  # @Date: 2022-06-06 15:51:19
- # @LastEditTime: 2022-06-18 15:27:59
+ # @LastEditTime: 2022-06-20 10:21:51
 ### 
 
 npm run build
 
 if [ $? -eq 0 ]; then
-
   npm publish --registry http://npm.sfjswl.com
 else
   echo "tsc编译异常, 请检查是否符合ts语法规范"
